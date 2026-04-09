cdd4b289095da757bfe7033a6826d1230e208b91
22mah22 <m.hovdar@gmail.com>
2026-04-09 23:45:12 +0200

    Use IApplicationPaths directly in GifController

---
+++ Jellyfin.Plugin.Template/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using System;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Http;
+global using IServerApplicationPaths = MediaBrowser.Common.Configuration.IApplicationPaths;
